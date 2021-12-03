aefaba139e459dcfec452e3fcab116234ec7fd9e
Kristoffer Hilmersson <kristofferhilmersson@MacBookCTO.local>
2021-12-03 07:54:43 +0100

    Day1: Second part done

--- day1/main.rs
+++ day1/main.rs
@@ -4,13 +4,26 @@ use std::path::Path;
 
 fn main() {
     // File hosts must exist in current path before this produces output
-    if let Ok(lines) = read_lines("./input.txt") {
-        first(lines);
+    if let Ok(reader) = read_lines("./input.txt") {
+        let lines_v: Vec<_> = reader.collect::<Result<_, _>>().unwrap();
+        second(lines_v);
     }
 }
 
-fn second(lines: io::Lines<io::BufReader<File>>) {
-    // rulla po 3
+fn second(lines: Vec<String>) {
+    let mut increased_measurement = 0;
+    for n in 2..lines.len()-1 {
+        let a1 = lines[n-2].parse::<i32>().unwrap();
+        let a2 = lines[n-1].parse::<i32>().unwrap();
+        let a3 = lines[n].parse::<i32>().unwrap();
+        let b1 = lines[n-1].parse::<i32>().unwrap();
+        let b2 = lines[n].parse::<i32>().unwrap();
+        let b3 = lines[n+1].parse::<i32>().unwrap();
+        if (b1+b2+b3) > (a1+a2+a3) {
+            increased_measurement += 1;
+        }
+    }
+    println!("Increased measurements: {}", increased_measurement);
 }
 fn first(lines: io::Lines<io::BufReader<File>>) {
     // Consumes the iterator, returns an (Optional) String
